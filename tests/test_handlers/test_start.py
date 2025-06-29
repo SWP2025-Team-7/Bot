4d5129f69046f5c9f67daf02a38c8bfa1843d0f0
Kazer0g <kazerog.workspace@gmail.com>
2025-06-29 22:53:18 +0300

    Save

--- tests/test_handlers/test_start.py
+++ tests/test_handlers/test_start.py
@@ -1,12 +1,22 @@
 import pytest
 
 from unittest.mock import AsyncMock
-
+from aiogram.fsm.context import FSMContext
+from aiogram.dispatcher.fsm.storage.base import StorageKey
 from bot.handlers import start_handler
+from tests.utils import TEST_USER, TEST_USER_CHAT
 
 @pytest.mark.asyncio
-async def test_start_handler():
+async def test_start_handler(storage, bot):
     message = AsyncMock()
-    await start_handler(message)
-
-    message.answer.assert_called_with()
+    state=FSMContext(
+        bot=bot,
+        storage=storage,
+        key=StorageKey(
+            bot_id=bot.id,
+            user_id=TEST_USER.id,
+            chat_id=TEST_USER_CHAT.id,
+        )
+    )
+    await start_handler(msg=message, state=state)
+    assert state.get_state() is None
