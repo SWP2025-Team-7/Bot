63189d6ad3d0b3fc1171b0bd222eeea03b8611d1
Kazer0g <kazerog.workspace@gmail.com>
2025-06-30 08:55:47 +0300

    Save

--- tests/test_handlers/test_cancel_handler.py
+++ tests/test_handlers/test_cancel_handler.py
@@ -1,8 +1,6 @@
 import pytest
 from unittest.mock import AsyncMock
-import datetime
 
-from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.base import StorageKey
 
@@ -16,13 +14,7 @@ from bot.handlers import cancel_handler
 
 @pytest.mark.asyncio
 async def test_cancel_handler(memory_storage, bot):
-    message = Message(
-        message_id=42,
-        date=datetime.datetime.now(),
-        text="/cancel",
-        chat=TEST_USER_CHAT,
-        from_user=TEST_USER
-    )
+    message = AsyncMock()
     state = FSMContext(
         storage=memory_storage,
         key=StorageKey(
@@ -34,4 +26,4 @@ async def test_cancel_handler(memory_storage, bot):
     await cancel_handler(msg=message, state=state)
     assert await state.get_state() == StudentStates.start
     # message.answer.assert_called_with(get_message(message=Messages.CANCEL, language=(await state.get_data()).get("language")))
-    message.answer.assert_called_with(get_message(message=Messages.INSTRUCTIONS, language=(await state.get_data()).get("language")))
+    message.answer.assert_called_with(f'{get_message(message=Messages.INSTRUCTIONS, language=(await state.get_data()).get("language"))}')
