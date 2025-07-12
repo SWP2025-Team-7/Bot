c636635bf1bc590ad1e4988573e094083d95f8d6
Kaz <90910163+Kazer0g@users.noreply.github.com>
2025-07-12 17:56:11 +0300

    Kazer0g (#26)
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Save
    
    * Stable
    
    * Save commit
    
    * Save commit

--- bot/bot_api/documents.py
+++ bot/bot_api/documents.py
@@ -2,17 +2,20 @@ import os
 import logging
 import json
 import requests as re
+from datetime import datetime
 
-def send_document(user_id, file_in_bytes):
+def send_document(user_id, file_path):
     logging.info(f"Upploading document from user ID:{user_id}")
     try :
         data = {
-            "userId": user_id,
-            "file_in_bytes": str(file_in_bytes, encoding="latin-1")
+            "user_id": user_id,
+            "file_path": file_path
         }
-        ans = re.get(f"http://{os.getenv('BACKEND_URL')}:8000/documents/upload", data=json.dumps(data),  headers={'Content-Type': 'application/json'})
+        ans = re.post(f"http://{os.getenv('BACKEND_URL')}:8000/users/{user_id}/documents/upload", data=json.dumps(data),  headers={'Content-Type': 'application/json'})
+        logging.info(f"Received data: {ans.json()}, code: {ans.status_code}")
+
     except Exception as e:
         logging.error(f"Connection Error while trying to uplad file from user ID:{user_id}")
         return False
-    return True
+    return ans.status_code, ans.json()
     
--- bot/bot_api/users.py
+++ bot/bot_api/users.py
@@ -3,15 +3,18 @@ import logging
 import json
 import requests as re
 
-def register_user(user_id, alias):
-    logging.info(f"Registring user ID:{user_id}, Alias:{alias}")
+def register_user(user_id, username):
+    logging.info(f"Registring user ID:{user_id}, Alias:{username}")
     try :
         data = {
-            "userId": user_id,
-            "alias": alias
+            "new_bot_user": {
+                "user_id": user_id,
+                "username": username
+            }
         }
-        ans = re.get(f"http://{os.getenv('BACKEND_URL')}:8000/users/register", data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        ans = re.post(url=f"http://{os.getenv('BACKEND_URL')}:8000/bot_users/register", data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        logging.info(f"Received data: {ans.json()}, code: {ans.status_code}")
     except Exception as e:
-        logging.error(f"Connection Error while trying to register user ID:{user_id}, Alias:{alias}")
+        logging.error(f"Connection Error while trying to register user ID:{user_id}, Alias:{username}")
         return False
-    return True
+    return ans.status_code, ans.json()
